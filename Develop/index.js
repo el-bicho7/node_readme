3c5e007fc749c4b2eeb5813e14c3080f9a2ab688
Cristian Loera <loera_73@hotmail.com>
2024-05-24 15:45:16 -0600

    Added readmeContent, writeToFile function

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,118 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const colors = require('colors');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: colors.brightCyan("What's your title?")
+  }, 
+  {
+    type: 'input',
+    name: 'description',
+    message: colors.brightCyan("Provide a short description explaining the what, why and how of your project:")
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: colors.brightCyan("Enter Installation instructions:")
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: colors.brightCyan("Enter Usage Information:")
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: colors.brightCyan("Contribution Guidelines:")
+  },  
+  {
+    type: 'input',
+    name: 'test',
+    message: colors.brightCyan("Test Instructions:")
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: colors.brightCyan('Choose a license:'),
+    choices: ['BSD 3-Clause License', 'GPL v3','MIT', 'None']
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: colors.brightCyan("Enter your GitHub username:")
+  }, 
+  {
+    type: 'input',
+    name: 'email',
+    message: colors.brightCyan("Enter your email:")
+  }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  const {title, description, installation, usage, contributing, test, license, github, email} = data;
+
+  const licenseBadge = generateMarkdown.renderLicenseBadge(license);
+  const licenseLink = generateMarkdown.renderLicenseLink(license);
+  const licenseSection = generateMarkdown.renderLicenseSection(license);
+
+
+  const readmeContent = `
+  # ${title}  ${licenseBadge}
+
+  ## Description 
+  ${description}
+
+  ## Table of Contents 
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Test](#test)
+  - [License](#license)
+  - [Questions](#questions)
+
+  ## Installation 
+  ${installation}
+
+  ## Usage
+  ${usage}
+
+  ## Contributing
+  ${contributing}
+
+  ## Test
+  ${test}
+
+  ## License
+  ${licenseSection}
+  ${licenseLink}
+
+  ## Questions
+  If you have any questions about the repository, contact me in ${github} and @ ${email}
+ 
+  `;
+
+
+  fs.writeFile(fileName, readmeContent, (err) =>{
+    err ? console.error(err) : console.log(`${fileName} created`);
+  });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer
+  .prompt(questions)
+  .then((data) => {
+    writeToFile('README.md', data);
+  });
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,14 +1,40 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const licenseBadges = {
+    'BSD 3-Clause License' : '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)',
+    'GPL v3' : '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)',
+    'MIT' : '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)',
+    'None' : ''
+  };
+
+  return licenseBadges[license];
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  const licenseLink = {
+    'BSD 3-Clause License' : '(https://opensource.org/licenses/BSD-3-Clause)',
+    'GPL v3' : '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)',
+    'MIT' : '(https://opensource.org/licenses/MIT)',
+    "None" : ''
+  }
+  return licenseLink[license];
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  const licenseSection = {
+    'BSD 3-Clause License' : '',
+    'GPL v3' : '',
+    'MIT' : '',
+    'None' : ''
+  };
+
+  return licenseSection[license];
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -17,4 +43,4 @@ function generateMarkdown(data) {
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = {renderLicenseBadge, renderLicenseLink, renderLicenseSection};
